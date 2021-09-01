5c4184b07c6babb4566aa2d14f381a8ce8cc46c0
Jihun Kim <zlhunzzing@gmail.com>
2021-09-01 16:12:37 +0900

    add react-native-ui-lib

---
+++ HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native-ui-lib';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
