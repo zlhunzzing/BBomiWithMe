056f943eed24418d574ad6d2ee1012b7d987902e
Jihun Kim <zlhunzzing@gmail.com>
2021-09-02 15:39:25 +0900

    add home ui

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Text, View } from 'react-native-ui-lib';
+import { View, Button } from 'react-native-ui-lib';
+import { StyleSheet, ImageBackground } from 'react-native';
+
+const defaultImg = require('./assets/icon.png')
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View flex>
+      <ImageBackground source={defaultImg} style={style.backgroundImg}>
+        <View style={style.menu}>
+          <Button style={style.btn} label="쓰다듬기" labelStyle={style.label} />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  backgroundImg: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  menu: {
+    marginBottom: 100,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    opacity: 0.6,
+  },
+  btn: {
+    backgroundColor: 'pink',
+    width: '70%'
+  },
+  label: {
+    color: 'black',
+    fontSize: 18
+  }
+});
